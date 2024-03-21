cf06c600c978e1af17f39d0e80636b0e68494bb8
Popov Stas <boss.staspopov@mail.ru>
2024-03-21 21:25:57 +0300

    Тест для получения комикса готов

--- tests/test_comics.py
+++ tests/test_comics.py
@@ -9,3 +9,17 @@ async def test_add_comics(ac: AsyncClient ) -> None:
 
     assert response.status_code == 200
 
+async def test_get_comics(ac: AsyncClient) -> None:
+    response = await ac.get('/api/comics/1/rating')
+    
+    assert response.status_code == 200
+    
+    data =  response.json()
+
+    assert data['comics_id'] == 1
+
+    assert data['title'] == 'TestComics'
+
+    assert data['author'] == 'TestAuthor'
+
+    assert len(data) == 4
